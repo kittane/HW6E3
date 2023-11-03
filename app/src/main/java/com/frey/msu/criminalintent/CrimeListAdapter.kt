fb468ae278c7af5b1f59f2bb57e423c3545ee644
Zack <zpf5s@login.missouristate.edu>
2023-11-03 01:42:56 -0500

    Completed exercise

--- app/src/main/java/com/frey/msu/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/frey/msu/criminalintent/CrimeListAdapter.kt
@@ -2,6 +2,7 @@ package com.frey.msu.criminalintent
 
 import android.app.LauncherActivity.ListItem
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -21,6 +22,12 @@ class CrimeHolder (
                 "${crime.title} clicked!",
                 Toast.LENGTH_SHORT
             ).show()
+        }
+
+        binding.crimeSolvedImg.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
         }
     }
 }
